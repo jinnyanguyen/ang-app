9036e3d0b4583f75cc26042a0154e795f8cb8c94
JinnyNguyen <jinnyanguyen@gmail.com>
2026-03-18 13:43:25 +0700

    Fix package.json for Heroku deployment

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve static Angular files
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/dist/ang-app"));
+
+// Route to Angular index file
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/ang-app/index.html"));
+});
+
+// Start server
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
